89998c11124c36e8bfccdd63d5d675ae8a0ec7e0
Jaehwan sin <fate427@naver.com>
2023-06-26 17:39:02 +0900

    230626 Oracle

--- hr23접속.sql
+++ hr23접속.sql
@@ -94,23 +94,23 @@ insert into emp (empno,ename,job,mgr,hiredate,sal,deptno) values ('7901','신재환
 
 --강사님
 INSERT INTO EMP VALUES(7369,'SMITH','CLERK',7902,TO_DATE('17-12-1980','dd-mm-yyyy'),800,NULL,20);
-INSERT INTO EMP VALUES (7499, 'ALLEN',  'SALESMAN',  7698,TO_DATE('20-02-1981', 'DD-MM-YYYY'), 1600,  300, 30);
-INSERT INTO EMP VALUES(7521, 'WARD',   'SALESMAN',  7698,TO_DATE('22-02-1981', 'DD-MM-YYYY'), 1250,  500, 30);
-INSERT INTO EMP VALUES(7566, 'JONES',  'MANAGER',   7839,TO_DATE('02-04-1981', 'DD-MM-YYYY'),  2975, NULL, 20);
-INSERT INTO EMP VALUES(7654, 'MARTIN', 'SALESMAN',  7698,TO_DATE('28-09-1981', 'DD-MM-YYYY'), 1250, 1400, 30);
-INSERT INTO EMP VALUES(7698, 'BLAKE',  'MANAGER',   7839,TO_DATE('01-05-1981', 'DD-MM-YYYY'),  2850, NULL, 30);
-INSERT INTO EMP VALUES(7782, 'CLARK',  'MANAGER',   7839,        TO_DATE('09-06-1981', 'DD-MM-YYYY'),  2450, NULL, 10);
-INSERT INTO EMP VALUES(7788, 'SCOTT',  'ANALYST',   7566,       TO_DATE('09-12-1982', 'DD-MM-YYYY'), 3000, NULL, 20);
-INSERT INTO EMP VALUES(7839, 'KING',   'PRESIDENT', NULL, TO_DATE('17-11-1981', 'DD-MM-YYYY'), 5000, NULL, 10);
-INSERT INTO EMP VALUES(7844, 'TURNER', 'SALESMAN',  7698, TO_DATE('08-09-1981', 'DD-MM-YYYY'),  1500, NULL, 30);
-INSERT INTO EMP VALUES(7876, 'ADAMS',  'CLERK',     7788,TO_DATE('12-01-1983', 'DD-MM-YYYY'), 1100, NULL, 20);
-INSERT INTO EMP VALUES(7900, 'JAMES',  'CLERK',     7698, TO_DATE('03-12-1981', 'DD-MM-YYYY'),   950, NULL, 30);
-INSERT INTO EMP VALUES (7902, 'FORD',   'ANALYST',   7566, TO_DATE('03-12-1981', 'DD-MM-YYYY'),  3000, NULL, 20);
-INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',     7782, TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300, NULL, 10);
+INSERT INTO EMP VALUES (7499, 'ALLEN',  'SALESMAN',7698,TO_DATE('20-02-1981', 'DD-MM-YYYY'), 1600,  300, 30);
+INSERT INTO EMP VALUES(7521, 'WARD',   'SALESMAN',7698,TO_DATE('22-02-1981', 'DD-MM-YYYY'), 1250,  500, 30);
+INSERT INTO EMP VALUES(7566, 'JONES',  'MANAGER',7839,TO_DATE('02-04-1981', 'DD-MM-YYYY'),  2975, NULL, 20);
+INSERT INTO EMP VALUES(7654, 'MARTIN', 'SALESMAN',7698,TO_DATE('28-09-1981', 'DD-MM-YYYY'), 1250, 1400, 30);
+INSERT INTO EMP VALUES(7698, 'BLAKE',  'MANAGER',7839,TO_DATE('01-05-1981', 'DD-MM-YYYY'),  2850, NULL, 30);
+INSERT INTO EMP VALUES(7782, 'CLARK',  'MANAGER',7839,TO_DATE('09-06-1981', 'DD-MM-YYYY'),  2450, NULL, 10);
+INSERT INTO EMP VALUES(7788, 'SCOTT',  'ANALYST',7566,TO_DATE('09-12-1982', 'DD-MM-YYYY'), 3000, NULL, 20);
+INSERT INTO EMP VALUES(7839, 'KING',   'PRESIDENT', NULL,TO_DATE('17-11-1981', 'DD-MM-YYYY'), 5000, NULL, 10);
+INSERT INTO EMP VALUES(7844, 'TURNER', 'SALESMAN',  7698,TO_DATE('08-09-1981', 'DD-MM-YYYY'),  1500, NULL, 30);
+INSERT INTO EMP VALUES(7876, 'ADAMS',  'CLERK',7788,TO_DATE('12-01-1983', 'DD-MM-YYYY'), 1100, NULL, 20);
+INSERT INTO EMP VALUES(7900, 'JAMES',  'CLERK',7698,TO_DATE('03-12-1981', 'DD-MM-YYYY'),   950, NULL, 30);
+INSERT INTO EMP VALUES (7902, 'FORD',   'ANALYST',7566,TO_DATE('03-12-1981', 'DD-MM-YYYY'),  3000, NULL, 20);
+INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',7782,TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300, NULL, 10);
 
-INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',     7782, TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300.54, NULL, 10);
-INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',     7782, TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300, NULL, 10);
-INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',     7782, TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300.5678, NULL, 10);
+INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',7782,TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300.54, NULL, 10);
+INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',7782,TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300, NULL, 10);
+INSERT INTO EMP VALUES(7934, 'MILLER', 'CLERK',7782,TO_DATE('23-01-1982', 'DD-MM-YYYY'), 1300.5678, NULL, 10);
 commit;
 
 --테이블 확인
@@ -160,7 +160,7 @@ select * from address2;
 select * from emp;
 select * from dept;
 
---aliasing 별칭
+--aliasing 별칭 단, select 문에만 사용가능하다.
 select * from emp;
 select empno from emp;-- emp테이블의 empno만 확인하겠다
 select empno as "사번" from emp;-- emp테이블의 empno가 사번으로 바뀜.
@@ -1941,7 +1941,79 @@ select ename,empno,deptno from emp where not deptno in ('20','30');
 select empno,ename,hiredate,deptno from emp where ename like 'S%';
 -- 20번 입사일이 81년도인 사람의 모든 정보를 출력하라.
 select * from emp where hiredate between to_date('1981-01-01','YY-MM-DD') and to_date('1981-12-31','YY-MM-DD');
-
+-- 21번 이름 중 S자가 들어가 있는 사람만 모든 정보를 출력하라.
+select * from emp where ename like '%S%'; 
+-- 22번 이름이 S로 시작하고 마지막 글자가 T인 사람의 모든 정보를 출력하라(단, 이름은 전체 5자리이다.)
+select * from emp where ename like 'S___T';
+-- 23번 첫 번째 문자는 관계없고, 두 번째 문자가 A인 사람의 정보를 출력하라.
+select * from emp where ename like '_A%';
+-- 24번 커미션이 NULL인 사람의 정보를 출력하라.
+select * from emp where comm is null;
+-- 25번 커미션이 NULL이 아닌 사람의 정보를 출력하라.
+select * from emp where comm is not null;
+-- 26번 부서가 30번 부서이고 급여가 $1,500 이상인 사람의 이름, 부서, 월급을 출력하라.
+select ename,empno,sal from emp where deptno = 30 and sal >= 1500;
+-- 27번 이름의 첫 글자가 K로 시작하거나 부서번호가 30인 사람의 사원번호, 이름, 부서번호를 출력하라.
+select empno,ename,deptno from emp where ename like 'K%' or deptno=30;
+-- 28번 급여가 $1500 이상이고 부서번호가 30번인 사원 중 직업이 MANAGER인 사람의 정보를 추력하라.
+select * from emp where sal>=1500 and deptno=30 and job='MANAGER';
+-- 29번 부서번호가 30인 사람 중 사원번호가 SORT.
+select empno from emp where deptno=30 group by empno;
+-- 30번 급여가 많은 순으로 SORT하라.
+select sal from emp group by sal order by sal desc;
+-- 31번 부서번호로 ASCENDING SORT  한 후 급여가 많은 사람 순으로 출력하라.
+select deptno,sal from emp group by deptno,sal order by deptno asc, sal desc;
+select * from emp order by deptno asc, sal desc;
+-- 32번 부서 번호가 DESCENDING SORT하고, 이름 순으로 ASCENDING SORT, 급여순으로 DESCENDING SORT 하라.
+select deptno,ename,sal from emp order by deptno desc, ename asc, sal desc;
+-- 33번 EMP TABLE 에서 이름, 급여, 커미션 금액, 총액 (SAL + COMM) 을 구하여 총액이 많은 순서로 출력하라. 
+-- 단 커미션이 NULL인 사람은 제외한다.
+select ename,sal,comm,sal+nvl(comm,0) from emp;
+-- 34번 10번 부서의 모든 사람들에게 급여의 13%를 보너스로 지불하기로 하였다. 이름, 급여, 보너스 금액, 부서번호를 출력하라.
+select ename,sal,sal*0.13 bonus,deptno from emp where deptno=10;
+-- 35번 30번 부서의 연봉을 계산하여 이름, 부서번호, 급여, 연봉을 출력하라.
+-- 단 연말에 급여의 150%를 보너스로 지급한다.
+select ename,deptno,sal,sal*0.15+(sal*12) yearsal from emp where deptno=30;
+-- 36번 부서번호가 20인 부서의 시간당 임금을 계산하여 출력하라.
+-- 단 1달의 근무일수가 12일이고, 1일 근무시간은 5시간이다.
+-- 출력양식은 이름, 급여, 시간당 임금(소수이하 첫 번째 자리에서 반올림)을 출력하라.
+select ename,sal,round((sal/12)/5,0) timesal from emp where deptno=20;
+-- 37번 급여가 $1,500 부터 $ 3,000 사이의 사람은 급여가 15%를 회비로 지불하기로 하였다.
+-- 이름, 급여, 회비(소수점 두 자리에서 반올림) 출력하라.
+select ename,sal,round(sal*0.15,1) fee from emp where sal>=1500 and sal<=3000;
+-- 38번 급여가 $2,000 이상인 모든 사람은 급여가 15%를 경보비로 내기로 하였다.
+-- 이름, 급여, 경조비 를 출력하라.
+select ename,sal,sal*0.15 fee from emp where sal>=2000;
+-- 39번 입사일 부터 지금까지의 날짜수를 출력하라. 부서번호, 이름, 입사일, 현재일, 
+-- 근무일수(소수점 이하 절삭), 근무년수, 근무월수(30일 기준), 근무주수를 출력하라.
+select deptno,ename,hiredate,sysdate,trunc((sysdate-hiredate),0) workdays,
+trunc((sysdate-hiredate)/365,0) workyears,trunc((sysdate-hiredate)/30,0) workmonth,
+trunc((sysdate-hiredate)/4,0) workweeks from emp;
+-- 40번 모든 사원의 실수령액을 계산하여 출력하라. 
+-- 단 급여가 많은 순으로 이름, 급여, 실수령액을 출력하라
+-- (실수령액은 급여에 대해 10%의 세금을 뺸 금액)
+select ename,sal,sal-(sal*0.1) netpay from emp order by sal desc;
+-- 41번 입사일로부터 90일이 지난 후의 사원이름, 입사일, 90일 후의 날, 급여를 출력하라.
+select ename,hiredate,hiredate+90,sal from emp;
+-- 42번 입사일로 부터 6개월이 지난 후의 입사일, 6개월 후의 날짜, 급여를 출력하라.
+select hiredate,add_months(hiredate,6),sal from emp;
+-- 43번 입사한 달의 근무일수를 계산하여 부서번호, 이름, 근무일수를 출력하라.
+select deptno,ename,last_day(hiredate)-hiredate workday from emp;
+-- 44번 모든 사원의 60일이 지난 후의 MONDAY 는 몇 년 , 몇 월, 몇 일 인가를 구하여
+-- 이름, 입사일, MONDAY를 출력하라.
+select ename,hiredate,next_day(add_months(hiredate,6),'월요일') from emp;
+-- 45번 입사일로부터 오늘까지의 일수를 구하여 이름, 입사일, 근무일수를 출력하라.
+select ename,hiredate,trunc(sysdate-hiredate,0) workday from emp;
+-- 46번 입사일을 1996년 5월 14일 의 형태로 이름, 입사일을 출력하라.
+select ename, to_char(hiredate,'YYYY-MM-DD') hiredate from emp;
+-- 47번 이름의 글자수가 6자 이상인 사람의 이름을 앞에서 3자리만 구하여 소문자로 이름만을 출력하라.
+select lower(ename) from emp where length(ename)>=6;
+-- 48번 10번 부서 월급의 평균, 최고, 최저, 인원수를 구하여 출력하라.
+select trunc(avg(sal),0) avg,max(sal) max,min(sal) min,count(sal) count from emp where deptno=10;
+-- 49번 각 부서별 급여의 평균, 최고, 최저, 인원수를 구하여 출력하라.
+select deptno,trunc(avg(sal),0) avg ,max(sal) max ,min(sal) min ,count(sal) count from emp group by deptno;
+-- 50번 각 부서별 같은 업무를 하는 사람의 인원수를 구하여 부서번호, 업무명, 인원수를 출력하라.
+select deptno,job,count(*) count from emp group by deptno,job order by deptno;
 -----------------------------------------------------------------------------230626---------------------------------------------------------------------------------------------
 -- 프로시저2 if문
 /
@@ -2262,7 +2334,40 @@ select count(*) from bigemp;
 --성능 테스트 테이블 준비
 select * from bigemp order by dbms_random.value;
 
+create table emp10
+as
+select * from bigemp order by dbms_random.value;
 
+create table emp_idx
+as
+select * from bigemp order by dbms_random.value;
+
+select count(*) from emp10;--index 없는 테이블
+select count(*) from emp_idx;--index 추가할 테이블
+
+--emp_idx 테이블에 index 추가하기
+create index idx_empidx_empno on emp_idx(emp_no);
+
+--테이블에 index 있는지 조회
+select * from user_indexes where table_name='EMP10';
+select * from user_indexes where table_name='EMP_IDX';
+
+--성능 비교
+--보통 index있는 테이블의 index 결과가 더 빨리 찾는다.
+select * from emp10 where emp_no<10100;--0.004초
+select * from emp_idx where emp_no<10100;--0.001~2초
+
+select * from emp10 where emp_no=11100;--0.012초
+select * from emp_idx where emp_no=11100;--0.005초
+
+--LOCK 기능 확인하기
+commit;
+select * from emp;
+
+--UPDATE 실행
+update emp set ename = 'ALLEN5' where empno=7499;
+
+commit;
 
 
 
