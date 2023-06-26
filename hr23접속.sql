082f8a7fa609efc1ca4159507f06abea41d7d006
Jaehwan sin <fate427@naver.com>
2023-06-26 18:03:43 +0900

    230626 Oracle

--- hr23접속.sql
+++ hr23접속.sql
@@ -1958,9 +1958,9 @@ select empno,ename,deptno from emp where ename like 'K%' or deptno=30;
 -- 28번 급여가 $1500 이상이고 부서번호가 30번인 사원 중 직업이 MANAGER인 사람의 정보를 추력하라.
 select * from emp where sal>=1500 and deptno=30 and job='MANAGER';
 -- 29번 부서번호가 30인 사람 중 사원번호가 SORT.
-select empno from emp where deptno=30 group by empno;
+select * from emp where deptno=30 order by empno;
 -- 30번 급여가 많은 순으로 SORT하라.
-select sal from emp group by sal order by sal desc;
+select * from emp order by sal desc;
 -- 31번 부서번호로 ASCENDING SORT  한 후 급여가 많은 사람 순으로 출력하라.
 select deptno,sal from emp group by deptno,sal order by deptno asc, sal desc;
 select * from emp order by deptno asc, sal desc;
@@ -1968,52 +1968,52 @@ select * from emp order by deptno asc, sal desc;
 select deptno,ename,sal from emp order by deptno desc, ename asc, sal desc;
 -- 33번 EMP TABLE 에서 이름, 급여, 커미션 금액, 총액 (SAL + COMM) 을 구하여 총액이 많은 순서로 출력하라. 
 -- 단 커미션이 NULL인 사람은 제외한다.
-select ename,sal,comm,sal+nvl(comm,0) from emp;
+select ename,sal,comm,sal+nvl(comm,0) "총액" from emp;
 -- 34번 10번 부서의 모든 사람들에게 급여의 13%를 보너스로 지불하기로 하였다. 이름, 급여, 보너스 금액, 부서번호를 출력하라.
 select ename,sal,sal*0.13 bonus,deptno from emp where deptno=10;
 -- 35번 30번 부서의 연봉을 계산하여 이름, 부서번호, 급여, 연봉을 출력하라.
 -- 단 연말에 급여의 150%를 보너스로 지급한다.
-select ename,deptno,sal,sal*0.15+(sal*12) yearsal from emp where deptno=30;
+select ename,deptno,sal,sal*0.15+(sal*12) "연봉" from emp where deptno=30;
 -- 36번 부서번호가 20인 부서의 시간당 임금을 계산하여 출력하라.
 -- 단 1달의 근무일수가 12일이고, 1일 근무시간은 5시간이다.
 -- 출력양식은 이름, 급여, 시간당 임금(소수이하 첫 번째 자리에서 반올림)을 출력하라.
-select ename,sal,round((sal/12)/5,0) timesal from emp where deptno=20;
+select ename,sal,round((sal/12)/5,0) "시간당 임금" from emp where deptno=20;
 -- 37번 급여가 $1,500 부터 $ 3,000 사이의 사람은 급여가 15%를 회비로 지불하기로 하였다.
 -- 이름, 급여, 회비(소수점 두 자리에서 반올림) 출력하라.
-select ename,sal,round(sal*0.15,1) fee from emp where sal>=1500 and sal<=3000;
+select ename,sal,round(sal*0.15,1) "회비" from emp where sal>=1500 and sal<=3000;
 -- 38번 급여가 $2,000 이상인 모든 사람은 급여가 15%를 경보비로 내기로 하였다.
 -- 이름, 급여, 경조비 를 출력하라.
-select ename,sal,sal*0.15 fee from emp where sal>=2000;
+select ename,sal,sal*0.15 "경조비" from emp where sal>=2000;
 -- 39번 입사일 부터 지금까지의 날짜수를 출력하라. 부서번호, 이름, 입사일, 현재일, 
 -- 근무일수(소수점 이하 절삭), 근무년수, 근무월수(30일 기준), 근무주수를 출력하라.
-select deptno,ename,hiredate,sysdate,trunc((sysdate-hiredate),0) workdays,
-trunc((sysdate-hiredate)/365,0) workyears,trunc((sysdate-hiredate)/30,0) workmonth,
-trunc((sysdate-hiredate)/4,0) workweeks from emp;
+select deptno,ename,hiredate,sysdate,trunc((sysdate-hiredate),0) "근무일수",
+trunc((sysdate-hiredate)/365,0) "근무년수",trunc((sysdate-hiredate)/30,0) "근무월수",
+trunc((sysdate-hiredate)/4,0) "근무주수" from emp;
 -- 40번 모든 사원의 실수령액을 계산하여 출력하라. 
 -- 단 급여가 많은 순으로 이름, 급여, 실수령액을 출력하라
 -- (실수령액은 급여에 대해 10%의 세금을 뺸 금액)
-select ename,sal,sal-(sal*0.1) netpay from emp order by sal desc;
+select ename,sal,sal-(sal*0.1) "실수령액" from emp order by sal desc;
 -- 41번 입사일로부터 90일이 지난 후의 사원이름, 입사일, 90일 후의 날, 급여를 출력하라.
-select ename,hiredate,hiredate+90,sal from emp;
+select ename,hiredate,hiredate+90 "90일 후의 날",sal from emp;
 -- 42번 입사일로 부터 6개월이 지난 후의 입사일, 6개월 후의 날짜, 급여를 출력하라.
-select hiredate,add_months(hiredate,6),sal from emp;
+select hiredate,add_months(hiredate,6) "6개월 후의 날짜",sal from emp;
 -- 43번 입사한 달의 근무일수를 계산하여 부서번호, 이름, 근무일수를 출력하라.
-select deptno,ename,last_day(hiredate)-hiredate workday from emp;
+select deptno,ename,last_day(hiredate)-hiredate "근무일수" from emp;
 -- 44번 모든 사원의 60일이 지난 후의 MONDAY 는 몇 년 , 몇 월, 몇 일 인가를 구하여
 -- 이름, 입사일, MONDAY를 출력하라.
-select ename,hiredate,next_day(add_months(hiredate,6),'월요일') from emp;
+select ename,hiredate,next_day(add_months(hiredate,6),'월요일') MONDAY from emp;
 -- 45번 입사일로부터 오늘까지의 일수를 구하여 이름, 입사일, 근무일수를 출력하라.
 select ename,hiredate,trunc(sysdate-hiredate,0) workday from emp;
 -- 46번 입사일을 1996년 5월 14일 의 형태로 이름, 입사일을 출력하라.
-select ename, to_char(hiredate,'YYYY-MM-DD') hiredate from emp;
+select ename, to_char(hiredate,'YYYY"년"MM"월"DD"일"') "입사일" from emp;
 -- 47번 이름의 글자수가 6자 이상인 사람의 이름을 앞에서 3자리만 구하여 소문자로 이름만을 출력하라.
 select lower(ename) from emp where length(ename)>=6;
 -- 48번 10번 부서 월급의 평균, 최고, 최저, 인원수를 구하여 출력하라.
-select trunc(avg(sal),0) avg,max(sal) max,min(sal) min,count(sal) count from emp where deptno=10;
+select trunc(avg(sal),0) avg,max(sal) max,min(sal) min,count(sal) "인원 수" from emp where deptno=10;
 -- 49번 각 부서별 급여의 평균, 최고, 최저, 인원수를 구하여 출력하라.
 select deptno,trunc(avg(sal),0) avg ,max(sal) max ,min(sal) min ,count(sal) count from emp group by deptno;
 -- 50번 각 부서별 같은 업무를 하는 사람의 인원수를 구하여 부서번호, 업무명, 인원수를 출력하라.
-select deptno,job,count(*) count from emp group by deptno,job order by deptno;
+select deptno,job,count(*) "인원 수" from emp group by deptno,job order by deptno;
 -----------------------------------------------------------------------------230626---------------------------------------------------------------------------------------------
 -- 프로시저2 if문
 /
